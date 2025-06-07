7f80530bafacf10c243f68b8fb4a7e2b0141d87e
Aldi Pramudya <llenndesu@gmail.com>
2025-06-08 00:43:54 +0700

    fix: xontolxuda temporary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { elysia } from "@/server/client"
-
 export default async function Home() {
-  const { data } = await elysia.api.get()
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <p className="text-4xl font-bold text-white">{data}</p>
+      <p className="text-4xl font-bold text-white">xontolxuda</p>
     </main>
   )
 }
